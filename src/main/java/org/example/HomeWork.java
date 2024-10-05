9de4ce99bb341360f0f61015fe09db6d40592aea
mmicheli <circe_vet@mail.ru>
2024-10-05 16:13:54 +0300

    answer

--- src/main/java/org/example/HomeWork.java
+++ src/main/java/org/example/HomeWork.java
@@ -1,6 +1,6 @@
 package org.example;
 
-import java.util.List;
+import java.util.*;
 
 
 public class HomeWork {
@@ -24,7 +24,68 @@ public class HomeWork {
      * Сигнатуру метода не меняем
      */
     public String findMaxSubstring(String str) {
-        //TODO реализовать метод
+        if (str == null) {
+            throw new NullPointerException("str must not be null");
+        }
+
+        ArrayList<String> subStrings = getSubstrings(str);
+
+        return getLongestString(subStrings);
+    }
+
+    private ArrayList<String> getSubstrings(String str) {
+        ArrayList<String> subStrings = new ArrayList<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            String item = getUniqueSubstr(str.substring(0, str.length() - i - 1));
+            if (item != null) {
+                subStrings.add(item);
+            }
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            String item = getUniqueSubstr(str.substring(i));
+            if (item != null) {
+                subStrings.add(item);
+            }
+        }
+
+        return subStrings;
+    }
+
+    private String getUniqueSubstr(String substr) {
+        LinkedHashSet<Character> unique = new LinkedHashSet<>();
+
+        for(char c : substr.toCharArray()) {
+            unique.add(c);
+        }
+
+        StringBuilder result = new StringBuilder();
+        for(char c : unique) {
+            result.append(c);
+        }
+
+        if (substr.contentEquals(result)) {
+            return result.toString();
+        }
+
+        return null;
+    }
+
+    private String getLongestString(ArrayList<String> strings) {
+        TreeSet<Integer> sizes = new TreeSet<>();
+
+        for (String s : strings) {
+            sizes.add(s.length());
+        }
+
+        int maxSize = sizes.last();
+        for (int i = 0; i < strings.size(); i++) {
+            if (strings.get(i).length() == maxSize) {
+                return strings.get(i);
+            }
+        }
+
         return null;
     }
 
@@ -34,13 +95,33 @@ public class HomeWork {
      * <br/>
      * Можно решать так же для английского алфавита, проверять что присутствуют буквы от A до Z хотя бы по одному разу.
      * <br/>
-     * A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+     * A pangram is a sentence that contains every single letter of the alphabet at least once. For example,
+     * the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z
+     * at least once (case is irrelevant).
      *
      * Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
      * @see <a href="https://www.codewars.com/kata/545cedaa9943f7fe7b000048">https://www.codewars.com/kata/545cedaa9943f7fe7b000048</a>
      */
     public boolean check(String sentence){
-        return false;
+        Map<Character, Integer> alphabeticCharSet = getUniqueSet();
+
+        for (char c : sentence.toLowerCase().toCharArray()) {
+            Integer i = alphabeticCharSet.get(c);
+            if (i != null) {
+                alphabeticCharSet.put(c, ++i);
+            }
+        }
+
+        return !alphabeticCharSet.containsValue(0);
     }
 
+    private Map<Character, Integer> getUniqueSet() {
+        HashMap<Character, Integer> charSet = new HashMap<>();
+
+        for (char ch = 'a'; ch <= 'z'; ++ch) {
+            charSet.put(ch, 0);
+        }
+
+        return charSet;
+    }
 }
--- src/test/java/org/example/BinaryIntSearchTreeTest.java
+++ src/test/java/org/example/BinaryIntSearchTreeTest.java
@@ -1,20 +1,96 @@
 package org.example;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BinaryIntSearchTreeTest {
 
+    HomeWork homeWork;
+
+    @BeforeEach
+    void setUp() {
+        homeWork = new HomeWork();
+    }
+    @ParameterizedTest(name = "Успешный поиск подстроки в строке: {arguments}")
+    @CsvSource({
+        "abcd, abcddcba",
+        "dcbaX, abcddcbaX",
+        "I love, I love Java",
+        "0987654321, 00090003330344453212345678900000987654321"
+    })
+    void findMaxSubstring_SuccessTest(String result, String input) {
+        assertEquals(result, homeWork.findMaxSubstring(input));
+    }
+
     @Test
-    void add() {
-        BinaryIntSearchTree tree = new BinaryIntSearchTree();
+    @DisplayName("Ошибка поиска уникальной подстроки: вместо строки передан null")
+    void findMaxSubstring_NullPointerException_FailTest() {
+        assertThrows(NullPointerException.class, () -> homeWork.findMaxSubstring(null));
+    }
 
-        tree.add(5);
+    @ParameterizedTest(name = "Успешная проверка строки на признак того, что она является панаграммой: {arguments}")
+    @CsvSource({
+        "abcdefghijklmnopqrstuvwxyz",
+        "abcdefghijklmMnNNNnnnnnOpqrstuvwxyz",
+        "6^;99|!!!!abcdefghijklmMnNNNnnnnnOpqrstuvwxyz"
+    })
+    void check_ReturnTrue_SuccessTest(String input) {
+        assertTrue(homeWork.check(input));
+    }
 
-        for (int i = 0; i < 10; i++) {
-            tree.add(i);
-        }
-        tree.add(10);
+    @ParameterizedTest(name = "Не успешная проверка строки на признак того, что она является панаграммой: {arguments}")
+    @CsvSource({
+            "abcdfghijklmMnNNNnnnnnOpqrstuvwxyz",
+            "abcdefghijklmnopqrstuvwxy",
+            "bcdefghijklmnopqrstuvwxyz",
+    })
+    void check_ReturnFalse_SuccessTest(String input) {
+        assertFalse(homeWork.check(input));
+    }
+
+    @Test
+    @DisplayName("Не успешная проверка строки на признак того, что она является панаграммой")
+    void check_ReturnFalseSpecialCases_FailTest() {
+        assertFalse(homeWork.check(""));
+        assertFalse(homeWork.check("adf;slk jvsgSFSAGDGkuhuksahf879687465546\njkhgfogfrg"));
+        assertFalse(homeWork.check("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
     }
 }
